6c433af745b31b2ba55874afd05052159c9a2557
sebastien deren laptop <sebastien.deren@gmail.com>
2023-03-30 16:45:15 +0200

    using bootstrap to make a proper register page

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -19,26 +20,22 @@ class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $builder
-            ->add('username',TextType::class,[
-            'constraints'=>[
-                new NotBlank(['message'=>'you have to enter a username']),
-                new Length(['min'=>6])
-            ]])
-            ->add('mail', EmailType::class)
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
+        $builder->setAttributes(['attr' => 'border border-dark rounded'])
+            ->add('username', TextType::class, [
                 'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
+                    new NotBlank(['message' => 'you have to enter a username']),
+                    new Length(['min' => 6])
                 ],
+
             ])
+            ->add('mail', EmailType::class)
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
+                'label_attr'=>['text-center'],
+                'label'=>'Password',
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Please enter a password',
@@ -51,7 +48,9 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
-        ;
+            ->add('submit', SubmitType::class, [
+                'attr' => ['btn-primary'],
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
