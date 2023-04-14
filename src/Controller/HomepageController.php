4092ef0f9d8a15c10a14811369550fa8cd834a2e
sebastien deren laptop <sebastien.deren@gmail.com>
2023-04-14 10:19:51 +0200

    some minor correction based upon symfony insight

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -4,11 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Message;
 use App\Form\MessageType;
-use App\Repository\MessageRepository;
-use App\Repository\UserRepository;
-
-use App\Service\CreateMessage;
-use App\Service\FrontMessage;
+use App\Service\MessageService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -19,9 +15,9 @@ class HomepageController extends AbstractController
     #[Route('/homepage', name: 'app_homepage', methods: ['GET', 'POST'])]
     #[Route('/', name: 'app_homepage', methods: ['GET', 'POST'])]
 
-    public function index(Request $request,CreateMessage $createMessage,FrontMessage $frontMessage): Response
+    public function index(Request $request,MessageService $messageService): Response
     {
-        $messages = $frontMessage();
+
         /*now we get all we'll need to make it so not all comment show either by hiding them with js or getting them little by little
         with a simple ajax request*/
         $message = new Message();
@@ -30,13 +26,13 @@ class HomepageController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $this->isGranted('IS_AUTHENTICATED');
-            $createMessage($message,$this->getUser());
+            $messageService->create($message,$this->getUser());
         }
-
+        $messages = $messageService->DisplayFront();
         return $this->render('homepage/index.html.twig', [
             'controller_name' => 'HomepageController',
             'messages' => $messages,
-            'formMessage' => $form
+            'formMessage' => $form,
         ]);
     }
 }
