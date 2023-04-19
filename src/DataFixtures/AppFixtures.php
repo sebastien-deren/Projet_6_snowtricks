2d71be4001f01f4cdd0b3f7fd8b4534a10a8ddde
sebastien deren laptop <sebastien.deren@gmail.com>
2023-04-19 12:55:22 +0200

    correcting conflicting files

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -74,11 +74,11 @@ class AppFixtures extends Fixture
             ->setSlug('one-foot-trick');
         $manager->persist($oneFootTrick);
 
-       for ($i=0;$i<5;$i++){
-           $comment=(new Message())->setContent($faker->text())
-               ->setUser(rand(0,1)?$green:$sebastien);
-           $manager->persist($comment);
-       }
+        for ($i=0;$i<5;$i++){
+            $comment=(new Message())->setContent($faker->text())
+                ->setUser(rand(0,1)?$green:$sebastien);
+            $manager->persist($comment);
+        }
 
 
 
