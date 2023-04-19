5fd6d12b977354bc44645433243ac575a1e7569e
sebastien deren laptop <sebastien.deren@gmail.com>
2023-04-19 10:40:02 +0200

    correcting conflicting files

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -41,6 +41,8 @@ class AppFixtures extends Fixture
             ->setSlug('mute');
         $manager->persist($mute);
 
+
+
         $oneHundredHeighty =(new Figure())
             ->setName('180')
             ->setDescription(' un demi-tour, soit 180 degrés d\'angle ')
@@ -71,7 +73,6 @@ class AppFixtures extends Fixture
 
        for ($i=0;$i<5;$i++){
            $comment=(new Message())->setContent($faker->text())
-
                ->setUser(rand(0,1)?$green:$sebastien);
            $manager->persist($comment);
        }
