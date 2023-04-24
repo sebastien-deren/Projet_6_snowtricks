d9276b53f2df5d397eba9955197ceee59e8412fd
sebastien deren laptop <sebastien.deren@gmail.com>
2023-04-24 10:10:05 +0200

    adding user authentication to Media CRUD

--- src/Controller/MediaController.php
+++ src/Controller/MediaController.php
@@ -11,8 +11,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('figure/{slug}/media')]
+#[IsGranted('ROLE_USER')]
 class MediaController extends AbstractController
 {
 
