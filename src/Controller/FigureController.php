709626389a2b5366ed691159cd8a1689038db989
sebastien deren laptop <sebastien.deren@gmail.com>
2023-04-25 11:25:21 +0200

    correcting conflicting files

--- src/Controller/FigureController.php
+++ src/Controller/FigureController.php
@@ -4,9 +4,11 @@ namespace App\Controller;
 
 use App\DTO\FigureFullDTO;
 use App\Entity\Figure;
+use App\Entity\Media;
 use App\Form\FigureType;
 use App\Repository\FigureRepository;
 use App\Service\FigureService;
+use App\Service\MediaService;
 use App\Entity\Message;
 use App\Form\MessageType;
 use App\Service\CreateMessage;
