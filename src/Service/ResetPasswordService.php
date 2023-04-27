b3fb491fa7d85a9adf55b6e82ae10463da9ee2a3
sebastien-deren <107032919+sebastien-deren@users.noreply.github.com>
2023-04-27 18:24:10 +0200

    Bugfix/resetpassword (#53)
    * broken try/catch
    
    * correcting resetpassword

--- src/Service/ResetPasswordService.php
+++ src/Service/ResetPasswordService.php
@@ -33,11 +33,9 @@ class ResetPasswordService
      */
     public function resetPasswordRequest(string $username, string $addressName): ?string
     {
-        try{
-
-            $user = $this->userRepository->findOneBy(['username' => $username]) ?? throw new UserNotFoundException();
-        }catch(UserNotFoundException $e){
-            $this->logger->alert($e);
+        $user = $this->userRepository->findOneBy(['username' => $username]);
+        if(!$user){
+            return $username.'@gmail.com';
         }
         $token = $user->getHash();
         $fqAddress = $this->urlGenerator->generate($addressName, ['id' => $user->getId(), 'token' => $token], UrlGeneratorInterface::ABSOLUTE_URL);
