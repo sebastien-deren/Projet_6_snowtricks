44db9af36e93947ff02aec963ecdc3050171f3ad
sebastien deren laptop <sebastien.deren@gmail.com>
2023-04-27 16:33:38 +0200

    Correcting carrousel to hide when there is less than the minimum items to "carousel" We just remove the bootstraps classes and add some flex element

--- public/asset/javascript/carroussel.js
+++ public/asset/javascript/carroussel.js
@@ -1,7 +1,21 @@
 window.addEventListener("DOMContentLoaded", (event) => {
     const items = document.querySelectorAll('.carousel-item')
+    const carousel = document.getElementById("carouselMedia")
+    const minPerSlide = 3
+    if(items.length<=minPerSlide && carousel){
+        carousel.classList.remove("carousel");
+        const carouselInner =document.getElementById("carousel-inner");
+        carouselInner.classList.remove("carousel-inner")
+        carouselInner.classList.add("d-flex");
+        carouselInner.classList.add('flex-col')
+        items.forEach((el)=>{
+            el.classList.remove('carousel-item');
+            el.classList.add('carousel-disable');
+        });
+    }
+    else{
     items.forEach((el) => {
-        const minPerSlide = 3
+
         let next = el.nextElementSibling
         for (let i=1; i<minPerSlide; i++) {
             if (!next) {
@@ -12,6 +26,6 @@ window.addEventListener("DOMContentLoaded", (event) => {
             el.appendChild(cloneChild.children[0])
             next = next.nextElementSibling
         }
-        console.log(next);
     })
+    }
 });
